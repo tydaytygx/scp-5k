ec46f39a40a4723c75cccce45db9de97277af4a8
TYGX <44126974+tydaytygx@users.noreply.github.com>
2025-09-04 15:45:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 GAME_DIR="/app/data"
 
 echo "INSTALL & UPDATE------------------------------"
-#/app/steamcmd/./steamcmd.sh +force_install_dir ${GAME_DIR} +login anonymous +app_update 884110 +quit
+/app/steamcmd/./steamcmd.sh +force_install_dir ${GAME_DIR} +login anonymous +app_update 884110 +quit
 
 echo "Start---------"
 /app/data/LinuxServer/PandemicServer.sh
